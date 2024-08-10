960fb6ae3937cd3c499421a1711e379a43cb973b
govindgore <govindgore714@gmail.com>
2024-08-10 11:56:19 +0530

    jay hind

--- Backend/bms/src/main/java/softgrid/bms/controller/CustController.java
+++ Backend/bms/src/main/java/softgrid/bms/controller/CustController.java
@@ -1,24 +1,5 @@
 package softgrid.bms.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import softgrid.bms.entity.Customer;
-import softgrid.bms.service.CustService;
-@RequestMapping("customer")
-@RestController
 public class CustController {
-	@Autowired
-	private CustService cSer;
-	
-	@PostMapping("newCustomer")
-	public ResponseEntity<?> addNewCustomer(@RequestBody Customer cust){
-			ResponseEntity<Object> customer= cSer.addNewCustomer(cust);
-			return customer;
-		
-	}
+
 }
---
+++ Backend/bms/src/main/java/softgrid/bms/entity/CurrentAccount.java
@@ -0,0 +1,14 @@
+package softgrid.bms.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class CurrentAccount {
+	
+	private int accountNo;
+	
+	
+	private Customer customer ;
+	
+
+}
